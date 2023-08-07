741ba582432fc16e438570b20229ec4fe1acbdcc
PrajwalApchundekar2023 <124507142+PrajwalApchundekar2023@users.noreply.github.com>
2023-08-07 15:17:09 +0530

    3 files modified, 11 added

--- src/main/main.component.ts
+++ src/main/main.component.ts
@@ -1,10 +1,13 @@
 import { Component, isStandalone, OnInit } from '@angular/core';
+import { ConversationComponent } from './conversation/conversation.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @Component({
   standalone: true,
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.css'],
+  imports: [SidenavComponent, ConversationComponent],
 })
 export class MainComponent implements OnInit {
   constructor() {}
